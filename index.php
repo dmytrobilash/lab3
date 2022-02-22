4c18edd678c048d5a43c316f266037e476763d51
dmytrobilash <dmutro.bilash@nure.ua>
2022-02-22 23:27:55 +0200

    before connecting keys

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ foreach($dbh->query($sqlSelect) as $cell)
 </form>
 
 
-<?php
+<?php //ВЫВОД ТРЕТЬЕЙ ЧАСТИ 
 $sqlSelect = "SELECT DISTINCT auditorium FROM iteh2lb1var2.lesson";
 echo '<form method="get" action="thirdDB.php">';
 echo "<p><strong>Вывести расписание для аудитории</strong> <select name='auditorium'>";
@@ -79,8 +79,10 @@ foreach($dbh->query($sqlSelect) as $cell)
 ?>
 </select>
 <input type='submit' value = 'ok'>
+</form>
 
 
+<!-- Добавление нового пз -->
 
 <p><b>Добавление нового ПЗ</b></p>
 <div id="div" class="changedP">
@@ -88,14 +90,14 @@ foreach($dbh->query($sqlSelect) as $cell)
 <form method="get" action="" id="form">
 
 <p>Введите день недели</p>
-<input required name = "week_day" value="Monday" form="form">
+<input required name = "week_day" value="Monday">
 <p>Введите номер пары</p>
-<input required name = "lesson_number" type="number" value="1" min="1" max="6" step="1" form="form">
+<input required name = "lesson_number" type="number" value="1" min="1" max="6" step="1">
 <p>Введите номер аудитории</p>
-<input required name="auditorium" value="104i" form="form">
+<input required name="auditorium" value="104i">
 <p>Введите название дисциплины</p>
-<input required name="disciple" value="Internet Technologies" form="form">
-<p><b> Выберите преподавателя<select name = "teacherSelection" form="form"><?php $sqlSelect = "SELECT * FROM iteh2lb1var2.teacher";
+<input required name="disciple" value="Internet Technologies" >
+<p><b> Выберите преподавателя<select name = "teacherSelection"><?php $sqlSelect = "SELECT * FROM iteh2lb1var2.teacher";
 echo "<option>Преподаватель</option>";
   
 foreach($dbh->query($sqlSelect) as $cell)
@@ -104,7 +106,7 @@ foreach($dbh->query($sqlSelect) as $cell)
     echo "</option>";
 }
 echo "</select>" ?>
- Выберите группу<select name ="groupSelection" form="form"><?php $sqlSelect = "SELECT * FROM iteh2lb1var2.groups";
+ Выберите группу<select name ="groupSelection" ><?php $sqlSelect = "SELECT * FROM iteh2lb1var2.groups";
 echo "<option>Группа</option>";
   
 foreach($dbh->query($sqlSelect) as $cell)
@@ -114,15 +116,15 @@ foreach($dbh->query($sqlSelect) as $cell)
 }
 echo "</select></b></p>" ?>
 
-<input type="submit" value ="Добавить"form="form">
+<input type="submit" value ="Добавить">
 
 </form>
 </div>
 
 
-
+<!-- НАЧАЛО ПХП ЛОГИКИ -->
 <?php
-$ID_Lesson = 5;
+
 $week_day = $_GET['week_day'];
 $lesson_number=$_GET['lesson_number'];
 $auditorium=$_GET['auditorium'];
@@ -136,16 +138,16 @@ try {
     // Устанавливаем корректную кодировку
     $dbh->exec("set names utf8");
     // Собираем данные для запроса
-    $data = array('ID_Lesson'=>$ID_Lesson,'week_day' => $week_day, 'lesson_number' => $lesson_number,
-    'auditorium'=>$auditorium, 'disciple'=>$disciple,
-    'type'=>$type
+   
     //'teacherSelection' => $teacherSelection,
     //'groupSelection' => $groupSelection
-    ); 
     
-    $sql = "INSERT INTO iteh2lb1var2.lesson (ID_Lesson, week_day, lesson_number, auditorium, disciple, type) values (?, ?, ?, ?, ?, ?)";
+    $alter = "ALTER TABLE $db.lesson CHANGE lesson.ID_Lesson lesson.ID_Lesson INT(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1";
+    $st= $dbh->prepare($alter);
+    $st->execute();
+    $sql = "INSERT INTO iteh2lb1var2.lesson (week_day, lesson_number, auditorium, disciple, type) values ( ?, ?, ?, ?, ?)";
     $stmt= $dbh->prepare($sql);
-    $stmt->execute([$ID_Lesson, $week_day, $lesson_number, $auditorium, $disciple, $type,]);
+    $stmt->execute([$week_day, $lesson_number, $auditorium, $disciple, $type]);
     
     $result = true;
 } catch (PDOException $e) {
