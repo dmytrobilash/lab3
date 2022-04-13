11d81082c4032a1050da0fca04624c5c9a2f6a03
dmytrobilash <dmutro.bilash@nure.ua>
2022-04-13 20:48:28 +0300

    end

--- index.php
+++ index.php
@@ -11,17 +11,12 @@
 <h2>Лабораторна робота №1, КІУКІ-19-5, Білаш Дмитро, Варіант №1 </h2>
 
 <?php
-
 $db = "iteh2lb1var2";
 $dsn = "mysql:host=localhost";
 $user = "root";
 $pass = "";
-
-
 $dbh = new PDO($dsn, $user, $pass);
 print("Connected to database <br>");
-
-
 
 
 //где то тут вывод первой части задания 
@@ -40,11 +35,6 @@ foreach($dbh->query($sqlSelect) as $cell)
 </select>
     <input type="submit" value = "ok">
 </form>
-
-
-
-
-
 
 
 <?php //Где то тут вывод второй части задания БОЖЕ ПОМОГИ МНЕ
@@ -96,8 +86,8 @@ foreach($dbh->query($sqlSelect) as $cell)
 <p>Введите номер аудитории</p>
 <input required name="auditorium" value="104i">
 <p>Введите название дисциплины</p>
-<input required name="disciple" >
-<p><b> Выберите преподавателя<select name = "teacher"><?php $sqlSelect = "SELECT * FROM iteh2lb1var2.teacher";
+<input required name="disciple" value="AK" >
+<p><b> Выберите преподавателя<select name = "name"><?php $sqlSelect = "SELECT * FROM iteh2lb1var2.teacher";
 echo "<option>Преподаватель</option>";
   
 foreach($dbh->query($sqlSelect) as $cell)
@@ -105,8 +95,9 @@ foreach($dbh->query($sqlSelect) as $cell)
     print_r($cell[1]);
     echo "</option>";
 }
+
 echo "</select>" ?>
- Выберите группу<select name ="groups" ><?php $sqlSelect = "SELECT * FROM iteh2lb1var2.groups";
+ Выберите группу<select name ="title" ><?php $sqlSelect = "SELECT * FROM iteh2lb1var2.groups";
 echo "<option>Группа</option>";
   
 foreach($dbh->query($sqlSelect) as $cell)
@@ -130,36 +121,64 @@ $lesson_number=$_GET['lesson_number'];
 $auditorium=$_GET['auditorium'];
 $disciple=$_GET['disciple'];
 $type = 'Practical';
-$teacher=$_GET['teacher'];
-$groups=$_GET['groups'];
+$name=$_GET['name'];
+$title=$_GET['title'];
 
 try {
-    
     // Устанавливаем корректную кодировку
     $dbh->exec("set names utf8");
     // Собираем данные для запроса
-   
-    //'teacherSelection' => $teacherSelection,
-    //'groupSelection' => $groupSelection
     
     $alter = "ALTER TABLE $db.lesson CHANGE lesson.ID_Lesson lesson.ID_Lesson INT(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1";
     $st= $dbh->prepare($alter);
     $st->execute();
+
     $sql = "INSERT INTO iteh2lb1var2.lesson (week_day, lesson_number, auditorium, disciple, type) values ( ?, ?, ?, ?, ?)";
     $stmt= $dbh->prepare($sql);
     $stmt->execute([$week_day, $lesson_number, $auditorium, $disciple, $type]);
     
-    // ВСТАВЛЯЕМ lesson_groups
-    $sql = "";
-    $stmt= $dbh->prepare($sql);
-    $stmt->execute();
+
+    // ВСТАВЛЯЕМ lesson_teacher
+    //Получаем айдишник препода
+    $sql = $dbh->prepare("SELECT * from $db.teacher where $db.teacher.name = :name");
+    $sql->execute(array('name' => $name));
+    $sql=$sql->fetch(PDO::FETCH_BOTH);
+    $teacher_id = $sql[0];
+
+    //получаем айдишник пары
+    $sql = $dbh->prepare("SELECT max(ID_Lesson) from $db.lesson");
     
-    // ВСТАВЛЯЕМ lesson_teachers
-    $sql = "INSERT INTO iteh2lb1var2.lesson_teachers () values ()";
-    $stmt= $dbh->prepare($sql);
-    $stmt->execute();
+    $sql->execute(array());
+    $sql=$sql->fetch(PDO::FETCH_BOTH);
+    $lesson_id = $sql[0];
+    
+    $sql = "INSERT INTO $db.lesson_teacher (FID_Teacher, FID_Lesson1) values ( ?, ?)";
+    $st = $dbh->prepare($sql);
+    $st->execute([$teacher_id, $lesson_id]);
+    
+    // ВСТАВЛЯЕМ lesson_groups
+    //получаем айдишник группы
+    $sql = $dbh->prepare("SELECT * from $db.groups where $db.groups.title = :title");
+    $sql->execute(array('title' => $title));
+    $sql=$sql->fetch(PDO::FETCH_BOTH);
+    $group_id = $sql[0];
+
+    //получаем айдишник пары
+    $sql = $dbh->prepare("SELECT max(ID_Lesson) from $db.lesson");
+    
+    $sql->execute(array());
+    $sql=$sql->fetch(PDO::FETCH_BOTH);
+    $lesson_id = $sql[0];
+
+    $sql = "INSERT INTO $db.lesson_groups (FID_Lesson2, FID_Groups) values ( ?, ?)";
+    $st = $dbh->prepare($sql);
+    $st->execute([$lesson_id, $group_id]);
+
+
+
 
     $result = true;
+
 } catch (PDOException $e) {
     
     print "Ошибка!: " . $e->getMessage() . "<br/>";
